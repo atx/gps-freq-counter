257ab979ed3b5082bf590061b2d3a32c9450078e
Josef Gajdusek <atx@atx.name>
2018-03-05 20:52:47 +0100

    fpga: Memory: Factor out memory connecting code

--- fpga/src/main/scala/Memory.scala
+++ fpga/src/main/scala/Memory.scala
@@ -4,33 +4,38 @@ package gfc
 import chisel3._
 import chisel3.util._
 
+object Memory
+{
+  def connectMemory(mem: SyncReadMem[Vec[UInt]], bus: MemoryBus) = {
+    val write = bus.valid && bus.wstrb =/= 0.U
+    val read = bus.valid && bus.wstrb === 0.U
+    var wordAddress = bus.addr >> 2
+    bus.ready := true.B
+
+    when (write) {
+      val inBitVec = Vec(Array(bus.wdata(7, 0),
+                               bus.wdata(15, 8),
+                               bus.wdata(23, 16),
+                               bus.wdata(31, 24)))
+      val inMask = Array(bus.wstrb(0),
+                         bus.wstrb(1),
+                         bus.wstrb(2),
+                         bus.wstrb(3))
+      mem.write(wordAddress, inBitVec, inMask)
+      bus.rdata := DontCare
+    } .otherwise {
+      bus.rdata := mem.read(wordAddress).toBits
+    }
+  }
+}
+
 class Memory(nwords: Int) extends Module {
   val io = IO(new Bundle {
     val bus = Flipped(new MemoryBus)
   })
 
-  io.bus.ready := true.B
-
-  val write = io.bus.valid && io.bus.wstrb =/= 0.U
-  val read = io.bus.valid && io.bus.wstrb === 0.U
-  var wordAddress = io.bus.addr >> 2
-
   val mem = SyncReadMem(nwords, Vec(4, UInt(8.W)))
-
-  when (write) {
-    val inBitVec = Vec(Array(io.bus.wdata(7, 0),
-                             io.bus.wdata(15, 8),
-                             io.bus.wdata(23, 16),
-                             io.bus.wdata(31, 24)))
-    val inMask = Array(io.bus.wstrb(0),
-                       io.bus.wstrb(1),
-                       io.bus.wstrb(2),
-                       io.bus.wstrb(3))
-    mem.write(wordAddress, inBitVec, inMask)
-    io.bus.rdata := DontCare
-  } .otherwise {
-    io.bus.rdata := mem.read(wordAddress).toBits
-  }
+  Memory.connectMemory(mem, io.bus)
 }
 
 class ReadOnlyMemory(data: Seq[UInt]) extends Module {
