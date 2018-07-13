dba0805cc9dcd61550bbfb4729a0590a2e5e95f1
Josef Gajdusek <atx@atx.name>
2018-07-13 19:13:43 +0200

    py: More user-friendly measurement mode

--- py/gfc.py
+++ py/gfc.py
@@ -3,7 +3,10 @@
 import collections
 import dateutil.parser
 import enum
+import math
+import pathlib
 import struct
+import time
 import usb.core
 import usb.util
 
@@ -63,30 +66,119 @@ class Device:
         return self.command_in(Command.SET_INPUT, arg1=int(select))
 
 
+def ansi_escape(code, *args):
+    return "\x1b[" + ";".join(map(str, args)) + code
+
+
+def do_config(dev, args):
+    print("Firmware build date '{}'".format(dev.build_date.isoformat(" ")))
+    if args.input:
+        print("Configuring input to '{}'".format(args.input))
+        dev.select_input(getattr(Device.Input, args.input.upper()))
+
+
+def do_raw(dev, args):
+    with args.output.open("w") as fout:
+        timestamp, value = None, None
+        while True:
+            new_timestamp, new_value = dev.fetch_measurement()
+            gps_timestamp, gps_has_fix, gps_n_sats = dev.fetch_gps_status()
+            if new_timestamp != timestamp:
+                print("{: 9d}     {: 9d}     {: 2d} {}"
+                      .format(new_timestamp, new_value, gps_n_sats, gps_has_fix),
+                      flush=True, file=fout)
+            else:
+                assert value == new_value, "What? Same timestamp but different values..."
+            timestamp, value = new_timestamp, new_value
+            time.sleep(0.2)
+
+
+def do_measure(dev, args):
+    total_dropped = 0
+    # This array will grow without bounds, intentional for now
+    measurements = []
+    print(ansi_escape("J", 2))  # Clear screen
+    with args.file.open("w") as fout:
+        timestamp = None
+        new_timestamp = None
+        while True:
+            time.sleep(0.2)
+            timestamp = new_timestamp
+            new_timestamp, value = dev.fetch_measurement()
+            if timestamp is None or new_timestamp == timestamp:
+                continue
+
+            fout.write("raw {} {}\n".format(new_timestamp, value))
+
+            if abs(new_timestamp - timestamp) > 1050:  # Magical constant!
+                total_dropped += 1
+                continue
+            measurements.append(value)
+            avg = sum(measurements) / len(measurements)
+
+            ppm = None
+            if args.target:
+                ppm = (avg - args.target) / args.target * 1e6
+            elif len(measurements) > 5:
+                # 100kHz rouding
+                args.target = round(avg, -5)
+
+            fout.write("processed {} {} {}\n".format(new_timestamp, value, ppm))
+
+            lines = [
+                "Last value:               {} Hz".format(measurements[-1]),
+                "Average value:            {:.6f} Hz".format(avg),
+                "Count (drop):             {: 4d} ({})".format(len(measurements), total_dropped),
+            ]
+            if ppm is not None:
+                lines.extend([
+                    "Error:                      {:.5f} ppm".format(ppm),
+                    "Target:                   {} Hz".format(args.target),
+                ])
+            max_length = max(map(len, lines))
+            lines = ["| " + l.ljust(max_length) + " |" for l in lines]
+            borders = "-" * (max_length + 2)
+            lines = ["/" + borders + "\\"] + lines + ["\\" + borders + "/"]
+
+            out = ansi_escape("H", 1, 1) + "\n".join(lines)
+            print(out)
+            fout.flush()
+
+
 if __name__ == "__main__":
     import argparse
-    import sys
-    import time
     # TODO: Make this into a proper script+package
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input", choices=["internal", "external"])
+    subparsers = parser.add_subparsers(dest="mode")
+    subparsers.required = True
+
+    parser_config = subparsers.add_parser("config")
+    parser_config.add_argument("-i", "--input", choices=["internal", "external"])
+
+    parser_raw = subparsers.add_parser("raw")
+    parser_raw.add_argument(
+        "-o", "--output",
+        type=pathlib.Path,
+        default=pathlib.Path("/dev/stdout")
+    )
+
+    parser_measure = subparsers.add_parser("measure")
+    parser_measure.add_argument(
+        "-f", "--file",
+        type=pathlib.Path,
+        default=pathlib.Path("/dev/null")
+    )
+    parser_measure.add_argument(
+        "-t", "--target",
+        type=float
+    )
+
     args = parser.parse_args()
 
-    gfc = Device()
-    print("Firmware build date '{}'".format(gfc.build_date.isoformat(" ")),
-          file=sys.stderr)
+    dev = Device()
 
-    if args.input:
-        gfc.select_input(getattr(Device.Input, args.input.upper()))
-
-    timestamp, value = None, None
-    while True:
-        new_timestamp, new_value = gfc.fetch_measurement()
-        gps_timestamp, gps_has_fix, gps_n_sats = gfc.fetch_gps_status()
-        if new_timestamp != timestamp:
-            print("{: 9d}     {: 9d}     {: 2d} {}"
-                  .format(new_timestamp, new_value, gps_n_sats, gps_has_fix), flush=True)
-        else:
-            assert value == new_value, "What? Same timestamp but different values..."
-        timestamp, value = new_timestamp, new_value
-        time.sleep(0.2)
+    {
+        "config": do_config,
+        "raw": do_raw,
+        "measure": do_measure,
+    }[args.mode](dev, args)
